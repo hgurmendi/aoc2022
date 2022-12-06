ea2c8f64fc341dfa4f636144185ae79d6e4667ec
Hernan Gurmendi <hgurmendi@kindalab.co>
2022-12-06 10:07:43 -0300

    Add resolution for the 6th day, part 2

--- aoc2022-06/index.ts
+++ aoc2022-06/index.ts
@@ -3,7 +3,8 @@ import { testInputs, realInput } from "./inputs";
 // A buffer is an array of single characters.
 type Buffer = string[];
 
-const MARKER_SIZE = 4;
+const START_OF_PACKET_MARKER_SIZE = 4;
+const START_OF_MESSAGE_MARKER_SIZE = 14;
 
 function parseLine(line: string): Buffer {
   return Array.from(line);
@@ -13,22 +14,32 @@ function parseMultipleInputs(input: string): Buffer[] {
   return input.split("\n").map(parseLine);
 }
 
-function isStartOfPacketMarker(marker: Buffer): boolean {
-  // const characters = new Set(Array.from(marker));
-  // return 4 === characters.size;
-  return new Set(marker).size === 4;
+function isStartOfPacketMarker(buf: Buffer): boolean {
+  return new Set(buf).size === START_OF_PACKET_MARKER_SIZE;
 }
 
-function getMarkerAt(buffer: Buffer, upToIndex: number): Buffer {
-  const fromIndex = Math.max(upToIndex - MARKER_SIZE + 1, 0);
+function isStartOfMessageMarker(buf: Buffer): boolean {
+  return new Set(buf).size === START_OF_MESSAGE_MARKER_SIZE;
+}
+
+function getMarkerAt(
+  buffer: Buffer,
+  upToIndex: number,
+  markerSize: number
+): Buffer {
+  const fromIndex = Math.max(upToIndex - markerSize + 1, 0);
   const toIndex = upToIndex + 1;
 
   return buffer.slice(fromIndex, toIndex);
 }
 
-function firstCharAfterStartOfPacket(buffer: Buffer): number {
+function firstCharAfterMarker(
+  buffer: Buffer,
+  isMarker: (buf: Buffer) => boolean,
+  markerSize: number
+): number {
   for (let i = 0; i < buffer.length; i++) {
-    if (isStartOfPacketMarker(getMarkerAt(buffer, i))) {
+    if (isMarker(getMarkerAt(buffer, i, markerSize))) {
       return i + 1;
     }
   }
@@ -40,17 +51,57 @@ function firstCharAfterStartOfPacket(buffer: Buffer): number {
 function testPart1(inputs: string): number[] {
   const testBuffers = parseMultipleInputs(testInputs);
 
-  return testBuffers.map(firstCharAfterStartOfPacket);
+  return testBuffers.map((buf) =>
+    firstCharAfterMarker(
+      buf,
+      isStartOfPacketMarker,
+      START_OF_PACKET_MARKER_SIZE
+    )
+  );
 }
-
-console.log('test part 1');
-console.log(testPart1(testInputs));
 
 function part1(input: string): number {
-  return firstCharAfterStartOfPacket(parseLine(input));
+  return firstCharAfterMarker(
+    parseLine(input),
+    isStartOfPacketMarker,
+    START_OF_PACKET_MARKER_SIZE
+  );
 }
 
+console.log("test part 1");
+console.log(testPart1(testInputs));
+// Correct answer: [7, 5, 6, 10, 11]
 
-console.log('part 1');
+console.log("part 1");
 console.log(part1(realInput));
 // Correct answer: 1896.
+
+// part 2
+
+function testPart2(inputs: string): number[] {
+  const testBuffers = parseMultipleInputs(testInputs);
+
+  return testBuffers.map((buf) =>
+    firstCharAfterMarker(
+      buf,
+      isStartOfMessageMarker,
+      START_OF_MESSAGE_MARKER_SIZE
+    )
+  );
+}
+
+function part2(input: string): number {
+  return firstCharAfterMarker(
+    parseLine(input),
+    isStartOfMessageMarker,
+    START_OF_MESSAGE_MARKER_SIZE
+  );
+}
+
+console.log("test part 2");
+console.log(testPart2(testInputs));
+// Correct answer: [19, 23, 23, 29, 26]
+
+console.log("part 2");
+console.log(part2(realInput));
+// Correct answer: 3452.
